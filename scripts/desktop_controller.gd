d04780ef6f50783bfab76fdbd76eba2615db3f89
Russell <rvssell.wa@gmail.com>
2026-07-31 13:33:44 +0700

    fix: Sprite passthrough

--- scripts/desktop_controller.gd
+++ scripts/desktop_controller.gd
@@ -5,12 +5,16 @@ var desktop_rect: Rect2i
 var last_poly: PackedVector2Array = []
 
 func _ready() -> void:
+	var window = get_window()
 	var current_screen = DisplayServer.window_get_current_screen()
 	desktop_rect = DisplayServer.screen_get_usable_rect(current_screen)
-	get_window().position = desktop_rect.position
-	get_window().size = desktop_rect.size
-	get_window().transparent = true 
-	get_window().transparent_bg = true
+	
+	window.mode = Window.MODE_WINDOWED
+	window.borderless = true
+	window.transparent = true 
+	window.transparent_bg = true
+	window.always_on_top = true
+	window.unfocusable = true
 	
 
 func get_left_boundary() -> float:
@@ -43,26 +47,37 @@ func _process(delta):
 	var has_hub = false
 
 	for pet in active_pets:
-		var coll = pet.get_node_or_null("CollisionShape2D")
-		if coll and coll.shape is RectangleShape2D:
-			var rect_size = coll.shape.size * pet.global_scale
-			var center_pos = coll.global_position
+		var sprite = pet.get_node_or_null("AnimatedSprite2D")
+		
+		if sprite and sprite.sprite_frames:
+			var texture = sprite.sprite_frames.get_frame_texture(sprite.animation, sprite.frame)
 			
-			var tl = (center_pos - (rect_size / 2.0)).round()
-			var tr = (tl + Vector2(rect_size.x, 0)).round()
-			var br = (tl + rect_size).round()
-			var bl = (tl + Vector2(0, rect_size.y)).round()
-			
-			if not has_hub:
-				hub_point = tl
-				has_hub = true
+			if texture:
+				var tex_size = texture.get_size()
+				var offset = sprite.offset
 				
-			poly.append(tl)
-			poly.append(tr)
-			poly.append(br)
-			poly.append(bl)
-			poly.append(tl)
-			poly.append(hub_point) # Trace back to hub so multiple pets connect!
+				if sprite.centered:
+					offset -= tex_size / 2.0
+					
+				var top_left = sprite.global_position + (offset * sprite.global_scale)
+				var rect_size = tex_size * sprite.global_scale
+				
+				var top_right = top_left + Vector2(rect_size.x, 0)
+				var bottom_right = top_left + rect_size
+				var bottom_left = top_left + Vector2(0, rect_size.y)
+				
+				if not has_hub:
+					hub_point = top_left
+					has_hub = true
+				
+				poly.append(top_left)
+				poly.append(top_right)
+				poly.append(bottom_right)
+				poly.append(bottom_left)
+				
+				# Close the square, and trace a zero-width line back to the Hub
+				poly.append(top_left)
+				poly.append(hub_point)
 
 	# Clean the array to prevent Windows crashes
 	var clean_poly = PackedVector2Array()
